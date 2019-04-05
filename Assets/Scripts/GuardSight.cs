82849c9b8f577239bc40cdc93eaa188a0d5a2688
Chris Zhang <clz16@case.edu>
2019-04-04 23:53:30 -0400

    finally got this dumbass ray cast to work

--- Assets/Scripts/GuardSight.cs
+++ Assets/Scripts/GuardSight.cs
@@ -5,17 +5,29 @@ using UnityEngine;
 public class GuardSight : MonoBehaviour
 {
 
+    LineRenderer lineRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
-       
-
-
+        lineRenderer = GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-      
+        //Debug.Log("transform.right = " + transform.right);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right, 100);
+        Debug.Log("Hit.point=" + hit.point);
+        Debug.Log("transform.position=" + transform.position);
+        Debug.DrawLine(transform.position, hit.point, Color.green);
+        Debug.Log(transform.right);
+        if(hit.collider != null) {
+           
+            //Debug.DrawRay(transform.position, hit.point, Color.green);
+        }
+
+        //transform.Rotate(0, 1, 0);
+        //this.transform.Rotate(Vector3.forward * 35 * Time.deltaTime);
     }
 }
