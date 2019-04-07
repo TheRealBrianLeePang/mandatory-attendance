42eebd956290150414802ccb764d9694f580eeac
unknown <jksmoove14@gmail.com>
2019-04-07 15:32:43 -0400

    Player movement + lasers

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+
+    public Transform startPoint;
+    public Transform endPoint;
+    private LineRenderer lr;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+        lr.SetWidth(.2f, .2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lr.SetPosition(0, startPoint.position);
+        lr.SetPosition(1, endPoint.position);
+    }
+}
---
+++ Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    private int _health;
+
+    void Start()
+    {
+        _health = 1;
+    }
+
+    public void Hurt(int damage)
+    {
+        _health -= damage;
+        Debug.Log("Health: " + _health);
+    }
+}
