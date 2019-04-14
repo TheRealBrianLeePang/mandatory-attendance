ff5eeb52b2ba2934ddfe84cd64f491d9e54b5ec6
Chris Zhang <clz16@case.edu>
2019-04-14 16:34:56 -0400

    updates to scenecontroller font and prefab

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class SceneController : MonoBehaviour
 {
     [SerializeField] private float timeRemaining;
-
+    [SerializeField] private Font font;
  
     void OnGUI()
     {
         timeRemaining -= Time.deltaTime;
         GUI.color = Color.black;
+        GUI.skin.font = font;
         GUI.Label(new Rect(250, 250, 1000, 1000), "Time remaining: " + timeRemaining.ToString());
         if (timeRemaining <= 0)
         {
