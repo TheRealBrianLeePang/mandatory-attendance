0336b43a2126ee3ae34ed4d7dfd221ee7dab08c2
Chris Zhang <clz16@case.edu>
2019-04-14 19:46:44 -0400

    added more terminal functionality

--- Assets/Scripts/TerminalBehavior.cs
+++ Assets/Scripts/TerminalBehavior.cs
@@ -8,13 +8,19 @@ public class TerminalBehavior : MonoBehaviour
     [SerializeField] GameObject controlledObject;
     [SerializeField] GameObject player;
     [SerializeField] float distance = 2.0f;
+    [SerializeField] bool shouldMakeActive = false;
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.Space) && Vector2.Distance(player.transform.position, transform.position) < distance)
+        if (shouldMakeActive)
         {
             controlledObject.SetActive(false);
+        }
+        if (Input.GetKey(KeyCode.Space) && Vector2.Distance(player.transform.position, transform.position) < distance)
+        {
+            controlledObject.SetActive(shouldMakeActive);
+            shouldMakeActive = !shouldMakeActive;
         }
     }
 }
